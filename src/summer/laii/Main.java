0d23ac95eedc8fc976188f1ae470f24eedd25099
Eric <eric.flores.7821@gmail.com>
2018-07-19 17:07:58 -0500

    Adding algorith for convert into postfix expression

--- src/summer/laii/Main.java
+++ src/summer/laii/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) {
         Gui gui = new Gui();
         gui.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        System.out.println("Test: ");
     }
 }
 
